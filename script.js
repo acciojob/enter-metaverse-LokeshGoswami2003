591e0efd6271718b14ad6350209e78037857c971
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-09-14 16:48:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 changed=()=>{
-	document.getElementById("status").outerHTML="<h1 id='status'>Entered Metaverse</h1>"
+	document.getElementById("status").innerHTML="<h1 id='status'>Entered Metaverse</h1>"
 }
